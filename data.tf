88278d79d830ed0fa056d96d8a4110e3a4d8f4b8
ZarakiKenpachi7 <shubhendu.upadhyay1@wpp.com>
2023-12-19 15:27:27 +0530

    Adding dependency to data blocks

--- data.tf
+++ data.tf
@@ -1,9 +1,13 @@
 data "tfe_workspace_ids" "all" {
   names        = ["*"]
   organization = "Shubhendu"
+
+  depends_on = [module.workspace1]
 }
 
 data "tfe_variables" "all_workspaces" {
   for_each     = toset(local.workspace_names)
   workspace_id = data.tfe_workspace_ids.all.ids[each.key]
+
+  depends_on = [data.tfe_workspace_ids.all]
 }
